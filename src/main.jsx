2f80a346c1c0428d971d02f248d32b2c13ba1f88
mdmhrz <razufreelence@gmail.com>
2025-05-30 02:09:07 +0600

    404 not ffound handling

--- src/main.jsx
+++ src/main.jsx
@@ -50,8 +50,13 @@ const router = createBrowserRouter([
         path: 'posts/:postId',
         loader: ({ params }) => fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`),
         Component: PostDetails
-      }
+      },
+
     ]
+  },
+  {
+    path: '*',
+    element: <h3>Not Found: 404 Status</h3>
   }
 ])
 
