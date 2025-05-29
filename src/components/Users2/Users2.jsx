7d09ef76903e40e23f1f8a8434a3249126e81c16
mdmhrz <razufreelence@gmail.com>
2025-05-29 18:07:11 +0600

    Users data loding with Loading effect and Suspense

---
+++ src/components/Users2/Users2.jsx
@@ -0,0 +1,15 @@
+import React, { use } from 'react';
+
+const Users2 = ({usersPromise}) => {
+    const users = use(usersPromise)
+    console.log('Users 2 suppense data load', users);
+    
+    return (
+        <div>
+            <h2>This is user 2
+            </h2>
+        </div>
+    );
+};
+
+export default Users2;
