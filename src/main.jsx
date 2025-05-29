b05141ed77a1bbe9da57d4b99e30bb5dfdf439ab
mdmhrz <razufreelence@gmail.com>
2025-05-29 15:35:17 +0600

    Route path learned

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <div>Hello from react router</div>
+    path:'/',
+    element: <div>Welcome to React Router World</div>
+  },
+  {
+    path: 'about',
+    element: <div>This is for about</div>
+  },
+  {
+    path: 'blogs',
+    element: <div>All my blogs are here</div>
+  },
+  {
+    path: 'contacts',
+    element: <h1>All my contacts are here</h1>
+  },
+  {
+    path: 'app',
+    Component: App
+  },
+  {
+    path: 'app2',
+    element: <App></App>
   }
 ])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
